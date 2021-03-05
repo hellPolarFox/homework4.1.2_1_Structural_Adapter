0318626b23065b1346c6b7296c63a80c910fe940
Ildar Akhmetkhanov <akhmetkhanovis@gmail.com>
2021-03-05 17:21:07 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Ints calc = new IntsCalculator();
+
+        System.out.println(calc.sum(5, 3));
+
+        System.out.println(calc.mult(5, 3));
+
+        System.out.println(calc.pow(5, 3));
+    }
+
+}
